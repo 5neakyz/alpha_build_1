f64de96bb2a165964ab993d9cbb13f0cd363841b
5neakyz <sneakyninja21212@gmail.com>
2024-07-02 11:46:49 +0100

    classless gui

--- src/gui/tungsten_gui.py
+++ src/gui/tungsten_gui.py
@@ -19,7 +19,7 @@ class TungstenGui(tk.Tk):
         super().__init__()
         # configure window
         self.title("5neakyz")
-        self.geometry(f"{1960}x{1080}")
+        self.geometry(f"{1460}x{760}")
 
         self.bind('<KeyPress>', self.onKeyPress)
 
@@ -43,14 +43,143 @@ class TungstenGui(tk.Tk):
         self.selected_comports = []
         self.selected_comports_str = tk.StringVar(value=self.selected_comports)
 
-        #frames / gui setup
-        self.menu_bar = MenuBar(self)
-        self.menu_bar = Sidebar(self)
-        self.main_area = MainArea(self)
-        self.info_bar = InfoBar(self)
+#frames / gui setup
+
+# Menu Bar
+
+        self.menu_bar = ttk.Frame()
+        self.menu_bar.pack(fill="x",side="top")
+        self.menu_bar_label = ttk.Label(self.menu_bar,background="#247F4C").pack(expand=True,fill="both")
+
+# Footer Info Bar
+        self.footer_bar = ttk.Frame()
+        self.footer_bar.pack(fill="x",side="bottom")
+        self.footer_bar_label = ttk.Label(self.footer_bar,background="#247F4C").pack(expand=True,fill="both")
+# Side bar
+
+        self.side_bar = ttk.Frame()
+        self.side_bar.pack(fill="y",side="left")
+        #create widgets
+        self.side_bar_run_btn  = ttk.Button(self.side_bar,text="Run",command=lambda: threading.Thread(target=self.run_btn_press).start())
+        self.side_bar_connect_btn = ttk.Button(self.side_bar,text="Connect")
+        self.side_bar_disconnect_btn = ttk.Button(self.side_bar,text="Disconnect")
+        self.side_bar_listbox = tk.Listbox(self.side_bar,listvariable=self.list_box_items,font=('',14),height=5,width=12)
+        self.side_bar_selected_devices_frame= ttk.LabelFrame(self.side_bar,text="Selected Devices")
+        self.side_bar_selected_devices_placeholder = ttk.Label(self.side_bar_selected_devices_frame,textvariable=self.selected_comports_str,wraplength=55)
+        #functions on click listbox
+        self.side_bar_listbox.bind('<<ListboxSelect>>', lambda event: self.items_selected(event))
+
+        #create grid
+        self.columnconfigure(1,weight=1)
+        self.rowconfigure((1,2,3,4,5,6,7,8,9),weight=0)
+
+        #place widgets
+        self.side_bar_run_btn.grid(row=1,column=1,sticky="new",padx=20, pady=(20, 0))
+        self.side_bar_connect_btn.grid(row=2,column=1,sticky="new",padx=20, pady=(20, 0))
+        self.side_bar_disconnect_btn.grid(row=3,column=1,sticky="new",padx=20, pady=(20, 0))
+        # list box
+        self.side_bar_listbox.grid(row=4,column=1,sticky="nw",padx=20, pady=(20, 0))
+        # alternate line colors
+        try:
+            for i in range(0,len(self.comports),2):
+                self.side_bar_listbox.itemconfigure(i, background='#242424')
+        except Exception as e: print(f'LIST BOX EXCEPTION{e}')
+
+        self.side_bar_selected_devices_frame.grid(row=5,column=1,sticky="new",padx=20, pady=(20, 0))
+        self.side_bar_selected_devices_placeholder.pack(padx=20,pady=20)
+
+# Main
+
+    # check buttons
+        self.check_buttons = ttk.Frame()
+        self.check_buttons.pack(fill="both")
+
+        self.check_buttons.columnconfigure((1,2,3,4),weight=0)
+        self.check_buttons.rowconfigure((1),weight=1)
+
+        #check options values
+        self.check_erase_config = tk.IntVar()
+        self.check_push_pers = tk.IntVar()
+        self.check_push_firm = tk.IntVar()
+        self.check_push_BLE = tk.IntVar()
+
+        self.check_1=ttk.Checkbutton(self.check_buttons, text="Erase Config",variable=self.check_erase_config).grid(row=1,column=1,padx=1,pady=1,sticky="w")
+        self.check_2=ttk.Checkbutton(self.check_buttons, text="Push Personality",variable=self.check_push_pers).grid(row=1,column=2,padx=1,pady=1,sticky="w")
+        self.check_3=ttk.Checkbutton(self.check_buttons, text="Push Firmware",variable=self.check_push_firm).grid(row=1,column=3,padx=1,pady=1,sticky="w")
+        self.check_4=ttk.Checkbutton(self.check_buttons, text="Push BLE",variable=self.check_push_BLE).grid(row=1,column=4,padx=1,pady=1,sticky="w")
+
+    # file selection 
+        self.file_selection = ttk.Frame()
+        self.file_selection.pack(fill="both")
+        #create grid on frame
+        self.file_selection.columnconfigure((1,2),weight=0)
+        self.file_selection.rowconfigure((1,2,3),weight=0)        
+
+        #vars
+        self.firmware_path = None
+        self.personality_path = None
+        self.ble_path = None
+        self.firmware_path_str = tk.StringVar(value=self.firmware_path)
+        self.personality_path_str = tk.StringVar(value=self.personality_path)
+        self.ble_path_str = tk.StringVar(value=self.ble_path)
+
+        #create widgets
+        self.select_firmware_btn  = ttk.Button(self.file_selection,text="Firmware",command=lambda: threading.Thread(target=self.get_path("firm")).start())
+        self.select_personality_btn  = ttk.Button(self.file_selection,text="Personality",command=lambda: threading.Thread(target=self.get_path("pers")).start())
+        self.select_ble_btn  = ttk.Button(self.file_selection,text="BLE",command=lambda: threading.Thread(target=self.get_path("ble")).start())
+        self.firmware_label = ttk.Label(self.file_selection,textvariable=self.firmware_path_str)
+        self.personality_label = ttk.Label(self.file_selection,textvariable=self.personality_path_str)
+        self.ble_label = ttk.Label(self.file_selection,textvariable=self.ble_path_str)
+
+        #place widgets
+        self.select_firmware_btn.grid(row=1,column=1,padx=2,pady=2)
+        self.select_personality_btn.grid(row=2,column=1,padx=2,pady=2)
+        self.select_ble_btn.grid(row=3,column=1,padx=2,pady=2)
+        self.firmware_label.grid(row=1,column=2,padx=2,pady=2,sticky="w")
+        self.personality_label.grid(row=2,column=2,padx=2,pady=2,sticky="w")
+        self.ble_label.grid(row=3,column=2,padx=2,pady=2,sticky="w")
+    #display tabs
+        self.tab_view = ttk.Notebook()
+        self.tab_view.pack(fill="both",expand=True,padx=5,pady=5)
+
+        self.new_pad = ttk.Frame(self.tab_view)
+
+        self.tab_view.add(self.new_pad,text="Tab 1")
+
+
 
 
         self.mainloop()
+
+
+    def get_path(self,type):
+        path = fd.askopenfilename()
+        head,tail = os.path.split(path)
+        if type == "firm":
+            self.firmware_path = tail
+            self.firmware_path_str.set(tail)
+        if type == "pers":
+            self.personality_path = tail
+            self.personality_path_str.set(tail)
+        if type == "ble":
+            self.ble_path = tail
+            self.ble_path_str.set(tail)
+        
+    def run_btn_press(self):
+        print(f'run')
+
+
+    def items_selected(self,event):
+        print(self.selected_comports)
+        try:
+            selected_item = event.widget.get(self.side_bar_listbox.curselection()[0])
+            print(selected_item)
+            if selected_item in self.selected_comports:
+                (self.selected_comports.remove(selected_item))
+            else:
+                (self.selected_comports.append(selected_item))
+            self.selected_comports_str.set(self.selected_comports)
+        except Exception as e: print(e)
 
     def resource_path(self,relative_path):
         """ Get absolute path to resource, works for dev and for PyInstaller """
@@ -95,207 +224,6 @@ class MenuBar(ttk.Frame):
 
     def donothing():
         pass
-
-class Sidebar(ttk.Frame):
-    def __init__(self,parent):
-        super().__init__(parent)
-        # create sidebar frame with widgets
-        #self.place(x=0,y=25,width=180,relheight=1)
-
-        self.pack(fill="y",side="left")
-
-        #create widgets
-        self.run_btn  = ttk.Button(self,text="Run",command=lambda: threading.Thread(target=self.run_btn_press(parent)).start())
-        self.connect_btn = ttk.Button(self,text="Connect")
-        self.disconnect_btn = ttk.Button(self,text="Disconnect")
-        self.listbox = tk.Listbox(self,listvariable=parent.list_box_items,font=('',14),height=5,width=12)
-        self.selected_devices_frame= ttk.LabelFrame(self,text="Selected Devices")
-        self.selected_devices_placeholder = ttk.Label(self.selected_devices_frame,textvariable=parent.selected_comports_str,wraplength=55)
-        #functions on click listbox
-        self.listbox.bind('<<ListboxSelect>>', lambda event: self.items_selected(event,parent))
-
-        #create grid
-        self.columnconfigure(1,weight=1)
-        self.rowconfigure((1,2,3,4,5,6,7,8,9),weight=0)
-
-        #place widgets
-        self.run_btn.grid(row=1,column=1,sticky="new",padx=20, pady=(20, 0))
-        self.connect_btn.grid(row=2,column=1,sticky="new",padx=20, pady=(20, 0))
-        self.disconnect_btn.grid(row=3,column=1,sticky="new",padx=20, pady=(20, 0))
-        # list box
-        self.listbox.grid(row=4,column=1,sticky="nw",padx=20, pady=(20, 0))
-        # alternate line colors
-        try:
-            for i in range(0,len(parent.comports),2):
-                self.listbox.itemconfigure(i, background='#242424')
-        except Exception as e: print(f'LIST BOX EXCEPTION{e}')
-
-        self.selected_devices_frame.grid(row=5,column=1,sticky="new",padx=20, pady=(20, 0))
-        self.selected_devices_placeholder.pack(padx=20,pady=20)
-
-    def run_btn_press(self,parent):
-        print(parent.main_area.radio_ml_options.radio_option.get())
-
-
-    def items_selected(self,event,parent):
-        print(parent.selected_comports)
-        try:
-            selected_item = event.widget.get(self.listbox.curselection()[0])
-            print(selected_item)
-            if selected_item in parent.selected_comports:
-                (parent.selected_comports.remove(selected_item))
-            else:
-                (parent.selected_comports.append(selected_item))
-            parent.selected_comports_str.set(parent.selected_comports)
-        except Exception as e: print(e)
-
-class MainArea(ttk.Frame):
-    def __init__(self,parent):
-        super().__init__(parent)
-        #ttk.Label(self,background="#242424").pack(expand=True,fill="both")
-        #self.place(x=180,y=25,relwidth=1,relheight=0.99)
-
-        self.pack(fill="both",anchor="nw")
-        self.config(style='red.TFrame')
-
-        self.radio_ml_options = CheckMLOption(self)
-        self.file_path_selection = FilePathSelection(self)
-        self.tabs = Tabs(self)
-        #self.device_display = DeviceDisplay(self)
-        #self.test = Test(self).pack(expand=True,fill="both")
-
-        #self.info_bar = InfoBar(self)
-
-    def create_widgets(self,parent):
-      pass  
-
-class CheckMLOption(ttk.Frame):
-    def __init__(self,parent):
-        super().__init__(parent)
-        #ttk.Label(self,background="red").pack(expand=True,fill="both")
-        #self.place(x=0,y=0,relwidth=1,height=35)
-        self.pack(side="top",anchor="nw",fill="x")
-        #self.config(style='red.TFrame')
-
-        self.columnconfigure((1,2,3,4),weight=0)
-        self.rowconfigure((1),weight=1)
-
-        #self.radio_option = tk.IntVar(value=1)
-        self.check_erase_config = tk.IntVar()
-        self.check_push_pers = tk.IntVar()
-        self.check_push_firm = tk.IntVar()
-        self.check_push_BLE = tk.IntVar()
-
-        self.check_1=ttk.Checkbutton(self, text="Erase Config",variable=self.check_erase_config)
-        self.check_2=ttk.Checkbutton(self, text="Push Personality",variable=self.check_push_pers).grid(row=1,column=2,padx=1,pady=1,sticky="w")
-        self.check_3=ttk.Checkbutton(self, text="Push Firmware",variable=self.check_push_firm).grid(row=1,column=3,padx=1,pady=1,sticky="w")
-        self.check_4=ttk.Checkbutton(self, text="Push BLE",variable=self.check_push_BLE).grid(row=1,column=4,padx=1,pady=1,sticky="w")
-
-        self.check_1.grid(row=1,column=1,padx=1,pady=1,sticky="w")
-
-    def get_radio_selection(self):
-        return self.radio_option.get()
-
-class FilePathSelection(ttk.Frame):
-    def __init__(self,parent):
-        super().__init__(parent)
-        
-        self.pack(side="top",anchor="nw",fill="x")
-
-        #create grid on frame
-        self.columnconfigure((1,2),weight=0)
-        self.rowconfigure((1,2),weight=0)        
-
-        #vars
-        self.firmware_path = None
-        self.personality_path = None
-        self.firmware_path_str = tk.StringVar(value=self.firmware_path)
-        self.personality_path_str = tk.StringVar(value=self.personality_path)
-
-        #create widgets
-        self.select_firmware_btn  = ttk.Button(self,text="Firmware",command=lambda: threading.Thread(target=self.get_firmware_path).start())
-        self.select_personality_btn  = ttk.Button(self,text="Personality",command=lambda: threading.Thread(target=self.get_personality_path).start())
-        self.firmware_label = ttk.Label(self,textvariable=self.firmware_path_str)
-        self.personality_label = ttk.Label(self,textvariable=self.personality_path_str)
-
-        #place widgets
-        self.select_firmware_btn.grid(row=1,column=1,padx=2,pady=2)
-        self.select_personality_btn.grid(row=2,column=1,padx=2,pady=2)
-        self.firmware_label.grid(row=1,column=2,padx=2,pady=2)
-        self.personality_label.grid(row=2,column=2,padx=2,pady=2)
-
-
-    def get_personality_path(self):
-        pPath = fd.askopenfilename()
-        self.personality_path = pPath
-        self.personality_path_str.set(pPath)
-
-    def get_firmware_path(self):
-        fPath = fd.askopenfilename()
-        self.firmware_path = fPath
-        self.firmware_path_str.set(fPath)
-
-class Test(ttk.Frame):
-    def __init__(self,parent):
-        super().__init__(parent)
-        self.columnconfigure((1,2,3,4),weight=0)
-        self.rowconfigure((1,2,3,4),weight=0)
-        self.lab1=ttk.Label(self,background="#ff3333")
-        self.lab2=ttk.Label(self,background="#aaffff")
-        self.lab3=ttk.Label(self,background="#bb1859")
-        self.lab4=ttk.Label(self,background="#765342")
-        self.lab1.pack(expand=True,fill="both")
-        self.lab1.grid(row = 1, column=1)
-        self.lab2.grid(row = 1, column=2)
-        self.lab3.grid(row = 2, column=1)
-        self.lab4.grid(row = 2, column=2)
-
-class DeviceDisplay(tk.Canvas):
-    def __init__(self,parent):
-        super().__init__(parent)
-        self.pack(expand=True,fill="both")
-
-        self.textBox = scrolledtext.ScrolledText(
-        self,
-        bg="#222222",
-        fg="#eeeeee",
-        border=0,
-        wrap="none",
-        highlightbackground="#247F4C",
-        highlightthickness=2,
-        font=("Sans", "10", "bold"),
-    )
-        
-        self.textBox.configure(padx=10, pady=10)
-        self.textBox.pack(expand=True,fill="both")
-        self.textBox.insert(tk.INSERT,"TEST")
-
-class InfoBar(ttk.Frame):
-    def __init__(self,parent):
-        super().__init__(parent)
-        self.pack(anchor="sw",side="bottom",fill="both")
-        self.config(style='green.TFrame')
-
-        self.columnconfigure((1,2),weight=0)
-        self.rowconfigure((1,2),weight=0)   
-
-        self.info_text = "Something"
-        self.info_text_str = tk.StringVar(value = self.info_text)
-        self.info_lbl = ttk.Label(self,textvariable=self.info_text_str)
-
-        self.info_lbl.grid(row=1,column=1,padx=2,pady=2)
-
-class Tabs(ttk.Notebook):
-    def __init__(self,parent):
-        super().__init__(parent)
-        self.pack(anchor="sw",side="bottom",fill="both")
-
-        self.tab1 = DeviceDisplay(self)
-        self.tab2 = DeviceDisplay(self)
-
-        self.add(self.tab1,text="Tab 1")
-        self.add(self.tab2,text="Tab 2")
-
 
 
 if __name__ == "__main__":
