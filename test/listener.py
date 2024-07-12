0c637feac1f8d69c01782dc191f57dc79e3b3027
5neakyz <sneakyninja21212@gmail.com>
2024-07-12 10:13:31 +0100

    listener changes

--- test/listener.py
+++ test/listener.py
@@ -16,7 +16,7 @@ class SerialPortManger():
         self.serial_port_baud = 115200
 
         try:
-            self.serial_port = serial.Serial(port = self.serial_port_name, baudrate=self.serial_port_baud, timeout=0.05)
+            self.serial_port = serial.Serial(port = self.serial_port_name, baudrate=self.serial_port_baud, timeout=2)
             self.serial_connection = True
             logging.info(f'Connection Established: {self.serial_port_name}')
         except Exception:
@@ -34,17 +34,12 @@ class Device(SerialPortManger):
         #only creates listener if there is connection
         if self.serial_connection:
             logging.info(f'1')
-            self.listener = Listener(self)
+            self.listener = Listen(self)
             self.listener.start_listening()
 
     def is_alive (self):
         if self.serial_connection == True:
-            for _ in range(1):
-                self.write_commands(["esc"])
-                time.sleep(0.5)
-                x = self.listener.get_buffer()
-                if "Main Menu" in str(x):
-                    return True
+            return True
         return False
 
     def write_commands(self,commands):
@@ -66,60 +61,42 @@ class Device(SerialPortManger):
 
 
 class Listener():
-    def __init__(self,device):
-        logging.info(f'creating Listener')
-        self.device = device
+    def __init__(self,*devices):
+        super().__init__()
+        self.devices = devices
         self.is_running = False
         self.needs_interrupt = False 
-        self.buffer_txt = ""
 
-    def get_buffer(self):
-        return self.buffer_txt
-    
     def interrupt(self):
         logging.info(f'Interrupting Listener')
         self.needs_interrupt = True
 
-    def start_listening(self):
-        logging.info(f'Starting Listener')
-        threading.Thread(target=self.listening).start()
-
-    def listening(self):
+    def listening(self,device):
+        logging.info(f'Listening')
         self.is_running = True
-        logging.info(f'Thread Started, Listening: {self.device.serial_port_name}')
+        logging.info(f'Thread Started, Listening: {device.serial_port_name}')
 
         while self.is_running and not self.needs_interrupt:
-            # line = self.device.serial_port.readline()
-            # if line:
-            #     logging.info(f'{self.device.serial_port_name} : {line}')
-
-            lines = self.device.serial_port.readlines()
-            if lines:
-                self.buffer_txt = lines
-                logging.info(f'{self.device.serial_port_name} : {self.format(lines)}')
-
-    def format(self,byte_array_list):
-        output = ""
-        for line in byte_array_list:
-            stripped = line.strip().replace(b'\t', b' ').replace(b'\r',b' ').replace(b'\x1b',b'').replace(b'\n',b'').replace(b'[2J',b'')
-            if stripped:
-                output += stripped.decode("utf-8") + '\n'
-        return output
+            line = device.serial_port.readline()
+            logging.info(f'{device.serial_port_name} : {line}')
+            #print(f'{device.serial_port_name} : {line}')
     
-if __name__ == '__main__':
+    def start_listening(self):
+        logging.info(f'Starting Listener')
+        results = []
+        with concurrent.futures.ThreadPoolExecutor() as executor:# parallelism 
+            tasks = [executor.submit(self.listening,device) for device in self.devices]
+            for x in concurrent.futures.as_completed(tasks):
+                results.append(x.result())
+        return results
+    
+    def format_readlines(self,lines):
+        output = ''
 
-    #self.button.clicked.connect(self.copy_text)
+        for line in lines:
+            text = line.strip().replace(b'\t', b'').replace(b'\n',b'').replace(b'\r',b'').replace(b'\x1b',b'').replace(b'\xfe',b'')
+            try:
+                output +=(text.decode('utf-8')+ ' \n')
+            except Exception as e: print(e,text)
 
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO,
-                        datefmt="%H:%M:%S")
-    print(f'setup unit')
-    sg14 = Device("COM19")
-    time.sleep(1)
-    print(f'ALIVE?: {sg14.is_alive()}')
-    time.sleep(1)
-    sg14.write_commands(['esc','4'])
-    time.sleep(10)
-    sg14.listener.interrupt()
-
-
+        return output
